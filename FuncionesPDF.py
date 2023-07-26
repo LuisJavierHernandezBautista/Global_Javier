212e569c673f2181f11dabd01cc46d130536b20a
LuisJavierHernandezBautista <luisjavierhdezbta@gmail.com>
2023-07-26 10:19:44 -0600

    global

--- FuncionesPDF.py
+++ FuncionesPDF.py
@@ -1,14 +1,20 @@
 from reportlab.pdfgen import canvas
+from FuncionesQR import *
 ruta = "C:/Users/yesen/OneDrive/Escritorio/funciones/"
 nombreArchivo = ruta + "reporteGlobal.pdf"
+nombreQR = ruta + "miQR.pdf"
 
 def generarPDF(listaNombres, listaEdades):
+    generarQR(nombreQR,"hola desde la funcion")
     c =canvas.Canvas(nombreArchivo)
     xInicial = 200
     yInicial = 700 
     c.setFont('Helvetica', 20)
     c.drawString(xInicial,yInicial,"edad")
     c.drawString(xInicial + 120,yInicial,"Nombre")
+    
+    c.drawImage(nombreQR,200,400,100,100)
+    
     for i in range(len(listaNombres)):
         c.setFont('Helvetica', 18)
         yInicial = yInicial - 20
---
+++ FuncionesQR.py
@@ -0,0 +1,8 @@
+import  qrcode
+#informacion QR
+def generarQR(nombreQR, informacion):
+    img = qrcode.make(informacion)
+    f = open(nombreQR,"wb")
+    img.save(f)
+    f.close()
+   
